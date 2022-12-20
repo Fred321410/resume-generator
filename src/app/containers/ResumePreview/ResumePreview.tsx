fd81b774fc8d481af97b6d1a960998c73830df18
Matthieu FLORENTIN <matthieu.florentin@gmail.com>
2022-12-20 15:17:05 +0100

    Print page

--- src/app/containers/ResumePreview/ResumePreview.tsx
+++ src/app/containers/ResumePreview/ResumePreview.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
 import './ResumePreview.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const ResumePreview = () => {
+  const print = () => {
+    const printContent = document.getElementById('content-to-print').innerHTML;
+    console.log(printContent);
+    const originalContent = document.body.innerHTML;
+    document.body.innerHTML = printContent;
+    window.print();
+    document.body.innerHTML = originalContent;
+  };
+
   return (
-    <div className="resume-preview">
-      <div className="resume-preview__header"></div>
-      <div className="resume-preview__content">
-        <div className="resume-preview__content__left"></div>
-        <div className="resume-preview__content__right"></div>
+    <div id="content-to-print">
+      <div className="resume-preview">
+        <div className="print-logo">
+          <FontAwesomeIcon
+            icon="print"
+            className="button__logo"
+            onClick={print}
+          />
+        </div>
+        <div className="resume-preview__header"></div>
+        <div className="resume-preview__content">
+          <div className="resume-preview__content__left">Left</div>
+          <div className="resume-preview__content__right">Right</div>
+        </div>
+        <div className="resume-preview__footer"></div>
       </div>
-      <div className="resume-preview__footer"></div>
     </div>
   );
 };
--- src/lib/graph/resolvers/resumes.ts
+++ src/lib/graph/resolvers/resumes.ts
@@ -9,8 +9,6 @@ module.exports = { Query }
 export {};
 
 async function getResumes(_: any, data: any) {
-  console.log('Hello');
   const resumes = await knex('resumes').select().where({userId: data.userId});
-  console.log(resumes);
   return resumes;
 }
