895c4f1d1b4b9af614d6f27987ca9dbb45afdc5e
Matthieu FLORENTIN <matthieu.florentin@gmail.com>
2023-01-30 10:59:31 +0100

    Add date-fns, some styling in experience block

--- src/app/components/Experience/Experience.tsx
+++ src/app/components/Experience/Experience.tsx
@@ -1,19 +1,33 @@
 import { Experiences } from '../../services/experiences.services';
 import React, { HTMLAttributes } from 'react';
 import './Experience.scss';
+import logoBemyapp from '../../logos/bemyapp.png';
+import { format } from 'date-fns';
+import { fr } from 'date-fns/locale';
 
 interface ExperienceProps extends HTMLAttributes<HTMLDivElement> {
   experience: Experiences;
 }
 
+function formatDate(date: string): string {
+  const formattedDate = format(new Date(date), 'MMMM uuuu', {
+    locale: fr,
+  });
+  return formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
+}
+
 const Experience = ({ experience }: ExperienceProps) => {
+  const datesAsString = experience.to
+    ? 'De ' + formatDate(experience.from) + ' à ' + formatDate(experience.to)
+    : 'A partir de ' + formatDate(experience.from);
   return (
     <div className="experience">
+      <img className="experience__logo" src={logoBemyapp} />
       <div className="experience__title">
         {experience.enterprise} - {experience.poste}
       </div>
       <div className="experience__subtitle">
-        {experience.from} - {experience.to} - {experience.city}
+        {datesAsString} - {experience.city}
       </div>
       <div className="experience__description">{experience.description}</div>
     </div>
--- src/lib/knex/seeds/03_experiences.js
+++ src/lib/knex/seeds/03_experiences.js
@@ -6,7 +6,7 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('experiences').del()
   await knex('experiences').insert([
-    {id: 1, poste: 'Lead Dev Fullstack', esn: '', enterprise: 'Bemyapp', from: '2020-11-01', to: '2022-12-01', stillInPost: false, logo: '', city: 'Nantes', country: '', description: 'This is a description', resume: 5},
+    {id: 1, poste: 'Lead Dev Fullstack', esn: '', enterprise: 'Bemyapp', from: '2020-11-01', to: '2022-12-01', stillInPost: false, logo: 'bemyapp.png', city: 'Nantes', country: '', description: 'Développement et maintenance de l\'application (code, infra). Participation aux choix techniques structurants et à leur intégration dans la roadmap. Conception technique, accompagnement de la réalisation et de la mise en production d\'une fonctionnalité majeure. 4 Personnes sur un an. Développement, en binôme avec un architecte AWS, de l\'agrégation des  données fonctionnelles et de leurs mises à disposition aux clients dans un dashboard. Restructuration et optimisation de la gamification sur la plateforme', resume: 5},
     {id: 2, poste: 'Lead Dev Fullstack', esn: '', enterprise: 'SIAO', from: '2020-11-01', to: '2022-12-01', stillInPost: false, logo: '', city: 'Nantes', country: '', description: 'This is a description', resume: 5},
     {id: 3, poste: 'Lead Dev Fullstack', esn: '', enterprise: 'Icowsoft', from: '2020-11-01', to: '2022-12-01', stillInPost: false, logo: '', city: 'Nantes', country: '', description: 'This is a description', resume: 5},
     {id: 4, poste: 'Lead Dev Fullstack', esn: '', enterprise: 'Generali', from: '2020-11-01', to: '2022-12-01', stillInPost: false, logo: '', city: 'Nantes', country: '', description: 'This is a description', resume: 5},
